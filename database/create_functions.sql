d6bd0f8f251810ab096befa280bf48cdf084b7d3
aloralove <aloralove89@gmail.com>
2022-06-06 13:52:58 -0800

    "assoc table works in workouts database"

--- database/create_functions.sql
+++ database/create_functions.sql
@@ -1,4 +1,4 @@
--- Takes in 3 parameters and returns an array containing the challenge you created
+-- Takes in #? parameters and returns an array containing the challenge you created
 CREATE OR REPLACE FUNCTION create_challenge(
     ch_name challenges.ch_name%type,
     descript challenges.descript%type
@@ -20,7 +20,7 @@ END;
  
 $$ LANGUAGE 'plpgsql';
 
--- Takes in 3 parameters and returns an array containing the nutrition you created
+-- Takes in #? parameters and returns an array containing the nutrition you created
 CREATE OR REPLACE FUNCTION create_nutrition(
     nu_name nutritions.nu_name%type,
     recipe challenges.descript%type
@@ -42,7 +42,7 @@ END;
  
 $$ LANGUAGE 'plpgsql';
 
--- Takes in 3 parameters and returns an array containing the workouts you created
+-- Takes in #? parameters and returns an array containing the workouts you created
 CREATE OR REPLACE FUNCTION create_workouts(
     wo_name workoutss.wo_name%type
 ) RETURNS SETOF workoutss AS $$
@@ -64,7 +64,7 @@ END;
 $$ LANGUAGE 'plpgsql';
 
 
-
+--assoc table for details to workouts one to many relationship
 
 CREATE OR REPLACE FUNCTION create_detail (
     uid permissions.workouts_id%type,
@@ -80,10 +80,25 @@ BEGIN
     VALUES (ttl, dsc)
     RETURNING id INTO detid;
 
-    INSERT INTO permissions (workouts_id, detail_id, permission) 
-    VALUES (uid, detid, 'Owner');
+    INSERT INTO permissions (workouts_id, detail_id) 
+    VALUES (uid, detid);
 
     RETURN detid;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+
+CREATE OR REPLACE FUNCTION create_permission (
+    uid permissions.workouts_id%type,
+    detid permissions.detail_id%type) 
+    
+    RETURNS void AS $$
+ 
+BEGIN
+    INSERT INTO permissions(workouts_id, detail_id)
+    VALUES (uid, detid);
+
 END;
 $$
 LANGUAGE 'plpgsql';
--- database/create_mock_data.sql
+++ database/create_mock_data.sql
@@ -1,37 +1,67 @@
 SET client_min_messages TO WARNING;
 
---CREATE OR REPLACE PROCEDURE create_mock_data()
---LANGUAGE plpgsql
---AS $$
 
---DECLARE
+CREATE OR REPLACE PROCEDURE create_mock_data()
+LANGUAGE plpgsql
+AS $$
+
+DECLARE
 
 -- Bunch of variables ...
---uid1 workouts.id%TYPE;
---uid2 workouts.id%TYPE;
---uid3 workouts.id%TYPE;
---uid4 workouts.id%TYPE;
---uid5 workouts.id%TYPE;
---uid6 workouts.id%TYPE;
---uid7 workouts.id%TYPE;
---uid8 workouts.id%TYPE;
+uid1 workoutss.id%TYPE;
+uid2 workoutss.id%TYPE;
+uid3 workoutss.id%TYPE;
+uid4 workoutss.id%TYPE;
+uid5 workoutss.id%TYPE;
+uid6 workoutss.id%TYPE;
+uid7 workoutss.id%TYPE;
+uid8 workoutss.id%TYPE;
 
---detid1 details.id%TYPE;
---detid2 details.id%TYPE;
---detid3 details.id%TYPE;
---detid4 details.id%TYPE;
---detid5 details.id%TYPE;
---detid6 details.id%TYPE;
---detid7 details.id%TYPE;
---detid8 details.id%TYPE;
+detid1 details.id%TYPE;
+detid2 details.id%TYPE;
+detid3 details.id%TYPE;
+detid4 details.id%TYPE;
+detid5 details.id%TYPE;
+detid6 details.id%TYPE;
+detid7 details.id%TYPE;
+detid8 details.id%TYPE;
 
---BEGIN
-
---PERFORM create_workouts('Arms');
-
---PEFROM create_detail(uid1, 'title', 'descript' ) 
+BEGIN
 
 -- Bunch of queries ...
+SELECT * FROM create_workouts('Arms') INTO uid1;
+SELECT * FROM create_workouts('HIIT') INTO uid2;
+SELECT * FROM create_workouts('Legs') INTO uid3;
+SELECT * FROM create_workouts('Fullbody') INTO uid4;
+SELECT * FROM create_workouts('Lowerbody') INTO uid5;
+SELECT * FROM create_workouts('Upperbody') INTO uid6;
+SELECT * FROM create_workouts('Back') INTO uid7;
+SELECT * FROM create_workouts('Yoga') INTO uid8;
+
+
+
+SELECT * FROM create_detail(uid1, 'title', 'descript' ) INTO detid1;
+SELECT * FROM create_detail(uid2, 'title', 'descript' ) INTO detid2;
+SELECT * FROM create_detail(uid3, 'title', 'descript' ) INTO detid3;
+SELECT * FROM create_detail(uid4, 'title', 'descript' ) INTO detid4;
+SELECT * FROM create_detail(uid5, 'title', 'descript' ) INTO detid5;
+SELECT * FROM create_detail(uid6, 'title', 'descript' ) INTO detid6;
+SELECT * FROM create_detail(uid7, 'title', 'descript' ) INTO detid7;
+SELECT * FROM create_detail(uid8, 'title', 'descript' ) INTO detid8;
+
+
+
+END;
+
+$$;
+-- Recreate our tables and functions
+--\i '/create_tables.sql';
+--\i '/create_functions.sql';
+
+-- Call the procedure that creates the data
+CALL create_mock_data ();
+
+
 SELECT * FROM create_challenge('30 days of Cardio', '1descript');
 SELECT * FROM create_challenge('30 days of Yoga', '2descript');
 SELECT * FROM create_challenge('30 days of push-ups', '3descript');
@@ -49,27 +79,3 @@ SELECT * FROM create_nutrition('Crispy Chipotle Shrimp Quesadilla', '5recipe');
 SELECT * FROM create_nutrition('Takeout-Level Chicken Fried Rice', '6recipe');
 SELECT * FROM create_nutrition('Oatmeal With Peanut Butter and Banana', '7recipe');
 SELECT * FROM create_nutrition('Protein Pancakes', '8recipe');
-
-
-SELECT * FROM create_workouts('Arms'); --INTO uid1;
-SELECT * FROM create_workouts('HIIT'); --INTO uid2;
-SELECT * FROM create_workouts('Legs'); --INTO uid3;
-SELECT * FROM create_workouts('Fullbody'); --INTO uid4;
-SELECT * FROM create_workouts('Lowerbody'); --INTO uid5;
-SELECT * FROM create_workouts('Upperbody'); --INTO uid6;
-SELECT * FROM create_workouts('Back'); --INTO uid7;
-SELECT * FROM create_workouts('Yoga'); --INTO uid8;
-
----SELECT * FROM create_detail(uid1, 'title', 'descript' ) INTO detid1;
---PERFORM create_permission(uid1, pid2, 'Collaborator');
-
---END;
-
---$$;
--- Recreate our tables and functions
---\i 'sql_scripts/create_tables.sql';
---\i 'sql_scripts/create_functions.sql';
-
--- Call the procedure that creates the data
---CALL create_mock_data ();
-
